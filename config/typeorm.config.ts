5672acb46d2baefdb2759fd62ad08770b9d7e6ee
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-03-05 04:07:44 -0600

    deploy: change configs

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -4,11 +4,11 @@ import { RefreshToken } from 'src/auth/entity/refreshToken.entity'
 import { UserSubscriber } from 'src/user/subscriber/user.subscriber';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  database: 'widget-demo-app',
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // port: 5432,
+  // database: 'widget-demo-app',
 
   synchronize: true,
   entities: [User, RefreshToken],
@@ -16,9 +16,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
 
 
-  // url: process.env.DATABASE_URL,
-  // ssl: true,
-  // extra: {
-  //   ssl: { rejectUnauthorized: false },
-  // },
+  url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: {
+    ssl: { rejectUnauthorized: false },
+  },
 };
